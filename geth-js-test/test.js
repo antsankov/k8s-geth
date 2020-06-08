e2ca21ae8ea32b229fe2ec65e2b25903238ea9db
root <antsankov@gmail.com>
2020-06-08 23:49:19 +0000

    working ws test

--- geth-js-test/test.js
+++ geth-js-test/test.js
@@ -2,8 +2,15 @@ var Web3 = require('web3');
 var web3 = new Web3()
 
 web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
-
-
-console.log('CURRENT BLOCK IS: ')
+web3.setProvider(new Web3.providers.WebsocketProvider('ws://localhost:8546'));
+console.log('++++++++++++++++++++++++++')
+console.log('RPC WORKS, CURRENT BLOCK ACCORDING TO RPC IS: ')
 web3.eth.getBlockNumber().then(console.log);
 
+const subscription = web3.eth.subscribe('newBlockHeaders', (error, blockHeader) => {
+if (error) return console.error(error);
+
+console.log('Successfully subscribed!', blockHeader);
+}).on('data', (blockHeader) => {
+console.log('data: ', blockHeader);
+});
